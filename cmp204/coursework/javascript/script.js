4fdcd97e9d1ca8a61771033f0febc9766d3ae67a
Blue <91186404+Bluemaster711@users.noreply.github.com>
2022-11-08 12:12:00 +0000

    updated

--- cmp204/coursework/javascript/script.js
+++ cmp204/coursework/javascript/script.js
@@ -1,3 +1,4 @@
+//used to greet incomming people
 window.onload=load_greeting;
 function load_greeting(){
 const d = new Date()
@@ -16,6 +17,7 @@ element.appendChild(para);}
 
 
 
+//used for creating intratctive nav
 $(document).ready(function(){
     $("#flip").hover(function(){
         $("#panel").slideToggle("show");
@@ -24,38 +26,34 @@ $(document).ready(function(){
 
 
 
+//random paragraph using math.random and AJAX 
 document.getElementById("randbutton").onclick=randpara;
 
-
-
-//loadDoc function
 function randpara() {
-	
-	//set-up the XMLHttpRequest object- gets data from the server behind the scenes
+
 	var xhttp = new XMLHttpRequest();
-	
-	//whenever the state changes, this function is called
+
 	xhttp.onreadystatechange = function() {
-	
-	//if the state happens to equal to 4 (request is finished/ready) and the HTTP status code is 200 (ok), do something to the page
+
     if (this.readyState == 4 && this.status == 200) {
     	
-    	
-    	//do something to the moreText div on the page
-    	//notice the responseText is stuff returned by the XMLHttpRequest
     	document.getElementById("randtext").innerHTML = this.responseText;
-    }//end if
-  };//end function
+    }
+  };
   
-
-  var num = str(Math.floor(Math.random() * 5 +1));
+  var num = (Math.floor(Math.random() * 5) + 1).toString();
   const exstend = ".txt";
   var finalfile = num + exstend;
-
-  //get request to server, find file.txt, true= asynchronous
   xhttp.open("GET", "txtfiles/" + finalfile, true);
   
-  
-  //send request to server
   xhttp.send();
+
+//   fetch("txtfiles/" + finalfile)
+//     .then((response) => {
+//         response.text()
+//     })
+//     .then((text) => {
+//         document.getElementById("randtext").innerHTML = text;
+//     });
+
 }
